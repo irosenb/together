5fd1c7eac5ebde4c336e6d05f40f587a677eecd5
Isaac Rosenberg <irosenb7@gmail.com>
2015-06-10 15:53:12 -0700

    gitignore

---
+++ Together/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Together
+//
+//  Created by Isaac Rosenberg on 6/8/15.
+//  Copyright (c) 2015 irosenb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Together/LoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.m
+//  Together
+//
+//  Created by Isaac Rosenberg on 6/8/15.
+//  Copyright (c) 2015 irosenb. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
